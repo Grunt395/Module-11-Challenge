6f584998df37d9dde37a6cbd05b497c3ca371ff6
Grunt395 <Grantlew@att.net>
2022-09-30 16:50:19 -0700

    Added hmtlRoute

---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const htmlRoute = require("./routes/htmlRoute");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.use("/", htmlRoute);
+
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
