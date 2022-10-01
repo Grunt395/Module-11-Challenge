8a9c4f9fd21d46065fc6858fa4a3e7473e0234fa
Grunt395 <Grantlew@att.net>
2022-09-30 17:21:18 -0700

    Added store.js and updated apiRoute,js

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const htmlRoute = require("./routes/htmlRoute");
-
+const apiRoute = require("./routes/apiRoute");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +10,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.use("/api", apiRoute);
 app.use("/", htmlRoute);
 
 
